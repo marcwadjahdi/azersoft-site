21df51607262e94069232af4b4088698c86f3d18
AZER SOFT <marc.wadjahdi.pro@gmail.com>
2024-09-28 16:17:54 +1100

    Remove public

--- public/assets/js/menu.js
+++
@@ -1,9 +0,0 @@
-const menu = {};
-const NAV_CLS = 'small-nav';
-const onWindowScroll = () => {
-    const $header = $(".header:first");
-    if(window.scrollY === 0) $header.removeClass(NAV_CLS);
-    else $header.addClass(NAV_CLS);
-}
-
-window.onscroll = () => onWindowScroll();
